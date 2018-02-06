9d9191f42de79f1147358ac573b1918428d7c069
alexandra.tolkina <alexandra.tolkina@gmail.com>
2018-02-06 16:12:03 +0300

    Finished with update summary for subtask

--- src/main/java/cloud/controllers/SubTaskController.java
+++ src/main/java/cloud/controllers/SubTaskController.java
@@ -9,17 +9,17 @@ import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class SubTaskController {
+    private final String uri = "/rest/api/2/issue/";
     @Autowired
     private AtlassianHostRestClients restClients;
-//    private final String uri ="/rest/api/2/issue/";
 
     @GetMapping(value = "/change-summary")
     public ModelAndView changeSummary(@RequestParam String issueKey) {
         ModelAndView model = new ModelAndView();
         model.setViewName("summary");
         model.addObject("issueKey", issueKey);
-//        Object issue = restClients.authenticatedAsAddon().getForObject(uri + issueKey, Object.class);
-//        model.addObject("issue", issue);
+        Object issue = restClients.authenticatedAsAddon().getForObject(uri + issueKey, Object.class);
+        model.addObject("issue", issue);
         return model;
     }
 }
