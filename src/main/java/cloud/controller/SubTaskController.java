4b3e714cc563b44d6e43bb8ceb3926584473f325
alexandra.tolkina <alexandra.tolkina@gmail.com>
2018-02-09 10:42:27 +0300

    Changed token

--- src/main/java/cloud/controller/SubTaskController.java
+++ src/main/java/cloud/controller/SubTaskController.java
@@ -3,18 +3,28 @@ package cloud.controller;
 import cloud.service.ProjectKeyService;
 import com.atlassian.connect.spring.AtlassianHostRestClients;
 import com.atlassian.connect.spring.AtlassianHostUser;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.stream.Collectors;
 
 @RestController
 public class SubTaskController {
     private final String uri = "/rest/api/2/issue/";
     private ProjectKeyService projectKeyService;
     private AtlassianHostRestClients restClients;
+    @Autowired
+    private RestTemplate restTemplate;
 
     public SubTaskController(ProjectKeyService projectKeyService, AtlassianHostRestClients restClients) {
         this.projectKeyService = projectKeyService;
@@ -51,19 +61,25 @@ public class SubTaskController {
     public ModelAndView config(@AuthenticationPrincipal AtlassianHostUser host) {
 //        HttpHeaders headers = new HttpHeaders();
 //        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+//        headers.set("Authorization", "Basic YWxleGFuZHJhLnRvbGtpbmFAZ21haWwuY29tOmZnSGpLbDE1Ng==");
 //
 //        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
 //        map.add("Add", "Add");
 //        map.add("addSelectValue", "true");
 //        map.add("addValue", "123456789");
-//        map.add("atl_token", "00c532d0-0dfd-4bf9-9fb5-ccdc561c868e|5c43a29060a891e312adea0e5c4a393f8339a8a1|lin");
+//        map.add("atl_token", "00c532d0-0dfd-4bf9-9fb5-ccdc561c868e|32586e0b48c960edbf3fa632de4069428a7c5758|lin");
 //        map.add("fieldConfigId", "1032");
 //        map.add("selectedParentOptionId", "");
-//        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(map, headers);
-
-        String request = "Add=Add&addSelectValue=true&addValue=123456789&atl_token=00c532d0-0dfd-4bf9-9fb5-ccdc561c868e|5c43a29060a891e312adea0e5c4a393f8339a8a1|lin&fieldConfigId=1032&selectedParentOptionId=";
-        String s = restClients.authenticatedAsAddon()
+//        HttpEntity<MultiValueMap<String, String>> requestMap = new HttpEntity<>(map, headers);
+//
+        String request = "Add=Add&addSelectValue=true&addValue=123456789&atl_token=00c532d0-0dfd-4bf9-9fb5-ccdc561c868e|32586e0b48c960edbf3fa632de4069428a7c5758|lin&fieldConfigId=1032&selectedParentOptionId=";
+//
+//        String s1 = restTemplate.postForObject("/secure/admin/EditCustomFieldOptions!add.jspa", requestMap, String.class);
+        String s = restClients.authenticatedAs(host)
                 .postForObject("/secure/admin/EditCustomFieldOptions!add.jspa", request, String.class);
+
+
+
 
 //        OkHttpClient client = new OkHttpClient();
 //
@@ -101,4 +117,57 @@ public class SubTaskController {
         projectKeyService.deleteSubTaskPluginEntity();
         return new ResponseEntity(HttpStatus.OK);
     }
+//    private String request(String targetURL, String body) {
+//
+//        HttpURLConnection connection = null;
+//
+//        try {
+//
+//            URL url = new URL(targetURL);
+//            connection = (HttpURLConnection) url.openConnection();
+//            connection.setRequestMethod("GET");
+//            connection.setRequestProperty("Content-Type",
+//                    "application/json; charset=utf-8");
+//
+//            connection.setRequestProperty("Authorization",
+//                    changeConfigService.get().getSapUrl());
+//
+//            OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream(), "UTF-8");
+//            writer.write(body);
+//            writer.close();
+//
+//
+//            if ((connection.getResponseCode() >= 200) && (connection.getResponseCode() <= 299)) {
+//
+//                InputStream is = connection.getInputStream();
+//                BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+//                String response;
+//                response = rd.lines().map(line -> line + '\r').collect(Collectors.joining());
+//                rd.close();
+//                return response;
+//
+//
+//            } else {
+//
+//                return null;
+//
+//            }
+//
+//
+//        } catch (Exception e) {
+//
+//            log.error("Connection to URL " + targetURL + " failed.");
+//            return null;
+//
+//        } finally {
+//
+//            if (connection != null) {
+//
+//                connection.disconnect();
+//
+//            }
+//
+//        }
+//
+//    }
 }
